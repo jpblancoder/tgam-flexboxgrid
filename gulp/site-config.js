d4b945131b17443cfa45f96b03efe9a41a5426dc
JP Blanchette <empirecoder@gmail.com>
2019-08-21 14:48:04 -0400

    Transfer from jpblancoder account to globeandmail organization

--- gulp/site-config.js
+++ gulp/site-config.js
@@ -4,5 +4,5 @@ module.exports = {
   // Must have leading slash and no trailing slash
   // Must match github repo name, so that when in github "docs" folder, urls will work.
   basePath: "/tgam-flexboxgrid",
-  distURL: "https://jpblancoder.github.io/tgam-flexboxgrid/dist/flexboxgrid.min.css"
+  distURL: "https://globeandmail.github.io/tgam-flexboxgrid/dist/flexboxgrid.min.css"
 };
